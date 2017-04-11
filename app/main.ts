539e7011be30e873adfcce29839ce92ba0caacf6
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2017-04-11 17:34:06 -0400

    Stop being dumb

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from "./app.module";
 
 import * as tnsOAuthModule from 'nativescript-oauth';
 var facebookInitOptions : tnsOAuthModule.ITnsOAuthOptionsFacebook = {
-  clientId: '1949423728625116',
-  clientSecret: '82b4c92d5a3f9cb0d3397683ce7c4abb',
+  clientId: 'man',
+  clientSecret: 'imdumb',
   scope: ['email'] //whatever other scopes you need
 };
 
